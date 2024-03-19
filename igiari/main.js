e0ca7ca6559060c7e5f5fe2a57ed1d0d2ed6817b
Senri <87857294+KurusuSenri@users.noreply.github.com>
2024-03-19 20:45:31 +0000

    add kurae takethat matta holdit

--- igiari/main.js
+++ igiari/main.js
@@ -1,24 +1,60 @@
 document.addEventListener("DOMContentLoaded", init, false);
 
-function init() {
-    var msg = document.querySelector("#message");
-    var audio = document.querySelector("#igiari_audio");
-    var image = document.querySelector("#igiari_image");
+let voiceType = 'none';
 
-    msg.innerHTML = "resources loaded, <b>please allow autoplay</b>"
+function init() {
+    let audio = document.querySelector("#igiari_audio");
+    let image = document.querySelector("#igiari_image");
+    let voiceTypeP = document.querySelector("#current_voice_type");
+    let imgSrc = "";
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === " ") {
+            console.log("change voice type");
+            if (voiceType === "none") {
+                voiceType = "jp-igiari";
+                audio.src = "igiari.mp3";
+                imgSrc = "igiari.png";
+            } else if (voiceType === "jp-igiari") {
+                voiceType = "en-objection";
+                audio.src = "objection.mp3";
+                imgSrc = "objection.png"
+            } else if (voiceType === "en-objection") {
+                voiceType = "jp-matta";
+                audio.src = "matta.mp3";
+                imgSrc = "matta.png";
+            } else if (voiceType === "jp-matta") {
+                voiceType = "en-holdit";
+                audio.src = "holdit.mp3";
+                imgSrc = "holdit.png";
+            } else if (voiceType === "en-holdit") {
+                voiceType = "jp-kurae";
+                audio.src = "kurae.mp3";
+                imgSrc = "kurae.png";
+            } else if (voiceType === "jp-kurae") {
+                voiceType = "en-takethat";
+                audio.src = "takethat.mp3";
+                imgSrc = "takethat.png";
+            } else if (voiceType === "en-takethat") {
+                voiceType = "none";
+            }
+            voiceTypeP.innerHTML = "current: " + voiceType;
+        }
+    });
 
     audio.addEventListener('playing', () => {
         console.log("playing");
-        image.src = ("igiari.jpg")
+        image.src = imgSrc;
     });
     audio.addEventListener('pause', () => {
         console.log("pause");
-        image.src = ("empty.png")
+        image.src = "empty.png";
     });
 
     document.addEventListener("mousemove", () => {
-        audio.play();
+        if (voiceType !== "none") {
+            audio.play();
+        }
     });
-
 
 }
